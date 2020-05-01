cb383c79861d45a74fde2033804cf80915d37caa
Yann Voté <ygversil@lilo.org>
2020-05-01 21:54:25 +0200

    [test] Fix flake8

--- extensible_celery_worker/__init__.py
+++ extensible_celery_worker/__init__.py
@@ -1,4 +1,5 @@
-"""extensible_celery_worker - Python Celery worker whose registered task list can be extended with plugins"""
+"""extensible_celery_worker - Python Celery worker whose registered task list can be extended with
+plugins"""
 
 __version__ = '0.1.0-dev1'
 __author__ = 'Yann Voté <ygversil@lilo.org>'
