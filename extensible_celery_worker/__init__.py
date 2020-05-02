e4c4b8d932dba01d7e92b778278bbdee70ee2730
Yann Voté <ygversil@lilo.org>
2020-05-02 14:51:59 +0200

    Bump version: 0.1.0-dev1 → 0.1.0-dev2

--- extensible_celery_worker/__init__.py
+++ extensible_celery_worker/__init__.py
@@ -1,7 +1,7 @@
 """extensible_celery_worker - Python Celery worker whose registered task list can be extended with
 plugins"""
 
-__version__ = '0.1.0-dev1'
+__version__ = '0.1.0-dev2'
 __author__ = 'Yann Voté <ygversil@lilo.org>'
 __all__ = []
 
